b3f458e48d40bd62a43c25e2e03433c32679b03a
김대현 <zcvfcat@gmail.com>
2023-01-13 09:15:03 +0900

    feat (warm-up)

---
+++ ex37.py
@@ -0,0 +1,8 @@
+def menu(**options):
+    while True:
+        option_string = '/'.join(sorted(options))
+        choice = input(f'Enter an option ({option_string}): ')
+        if choice in options:
+            return options[choice]()
+
+        print('Not a valid option')
