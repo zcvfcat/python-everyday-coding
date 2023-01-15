46a8f1029ff29ee172da6ac69450a9948598bbb4
zcvfcat <zcvfcat@gmail.com>
2023-01-15 18:37:24 +0900

    feat (warm-up)

---
+++ ex46.py
@@ -0,0 +1,19 @@
+class MyEnumerate:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+
+        value = (self.index, self.data[self.index])
+        self.index += 1
+        return value
+
+
+for index, one_item in MyEnumerate('abcd'):
+    print(f'{index} : {one_item}')
